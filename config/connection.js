56c002eb04d4dd25ff0d405ff8cc73a9c8ab2a63
ficklement <michaelcosand@gmail.com>
2024-02-12 19:39:47 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 let sequelize;
 
@@ -6,15 +7,37 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
+    'project2_db',
+    'root',
+    'root',
     {
-      host: 'kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
-    },
+      port: 3306
+    }
   );
 }
 
 module.exports = sequelize;
+
+
+// const Sequelize = require('sequelize');
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//     },
+//   );
+// }
+
+// module.exports = sequelize;
