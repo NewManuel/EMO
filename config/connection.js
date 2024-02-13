9669f5ca228d34c070867d68c6ee3f22cce28bde
ficklement <michaelcosand@gmail.com>
2024-02-12 18:48:03 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -7,14 +7,14 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    'project2_db',
-    'root',
-    '',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
-    }
+      port: 3306,
+    },
   );
 }
 
